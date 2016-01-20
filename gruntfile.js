3ab828ee712f9d1ab4573638bd6b95ab9494d2be
Julio Sena <julio.657.js@gmail.com>
2016-01-20 10:04:44 -0500

    cambiosn en grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -66,6 +66,25 @@ module.exports = function(grunt) {
                 },
             all: paths.js
         },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: 'rc',
+                metadata: '',
+                regExp: false
+            }
+        },
         uglify: {
          core: {
               options: {
